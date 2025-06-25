ed40a38a767a8adc33bfea88dfa2ba4f57c05a07
Sandilem7789 <sandilem7789@gmail.com>
2025-06-25 18:20:11 +0200

    loops done

--- loops.py
+++ loops.py
@@ -6,11 +6,15 @@
 
 print("For Loop Example:")
 # This loop will iterate through each fruit in the list and print it
-fruits = ["apple", "banana", "cherry", ["kiwi", "mango"]]
+
+print("\nWhile Loop Example: String Iteration")   
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry"]
 
 for fruit in fruits:
     print(fruit)
-    
+
+print("\nWhile Loop Example: Number Iteration")   
 numbers = [1, 2, 3, 4, 5]
 for number in numbers: 
     print(number)
@@ -24,3 +28,18 @@ print("\nWhile Loop Example:")
 count = 0
 while count < 5:
     print("Count is:", count, " - Looping...")
+    count += 1
+    # If count reaches 5, break the loop
+    if count == 5:
+        print("Count is equal to 5, breaking the loop.")
+        break
+    
+#This loops counts down from 10 to the number between 10 and zero, the input will be requested from the user
+print("\nWhile Loop Example: Countdown")
+countdown_start = int(input("Enter a number between 0 and 10 to start the countdown: "))
+while countdown_start >= 0:                                                                                 # Loop until countdown_start is less than 0
+    print("Countdown:", countdown_start)
+    countdown_start -= 1
+    if countdown_start < 0:
+        print("Countdown finished!")
+        break
